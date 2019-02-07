6c94cdfef629d88e5372f339d43c4f00d61b1eb1
samvaio <samdivaio@icloud.com>
2019-02-07 10:53:50 +0530

    Windows Support

---
+++ moderators/patch.bat
@@ -0,0 +1,21 @@
+@ECHO off
+TITLE Downloading NadekoBot Extension...
+SET "root=%~dp0"
+CD /D "%root%"
+ECHO Downloading...
+ECHO.
+::File
+SET "FILENAME=%~dp0\NadekoBot\NadekoBot.Core\Modules\Administration\UserPunishCommands.cs"
+::URL with raw.githubusercontent.com
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/samdivaio/NadekoBot-Extensions/master/moderators/UserPunishCommands.cs -OutFile '%FILENAME%'"
+GOTO end
+:end
+	::Normal execution of end of script
+	TITLE NadekoBot Extension Installation complete!
+	ECHO NadekoBot Extension Downloaded.
+	CD /D "%root%"
+	ECHO.
+	ECHO Installation complete!
+	ECHO.
+	timeout /t 5
+	del patch.bat
